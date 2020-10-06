5f53bcdb6517765670f22d522815f314c7aadb3a
Roderick Montague <roderick.montague243@gmail.com>
2020-10-05 20:51:22 -0400

    Variables for Character Differences
    Added variables for character differences. Currently they don't do anything.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,11 +9,16 @@ public class Player : MonoBehaviour
     public float movementSpeed = 10.0F;
     public bool momentumMovement = false;
 
+    protected float speedMult = 1.0F;
+    protected float knockbackMult = 1.0F;
+    protected float jumpMult = 1.0F;
+    protected float sturdyMult = 1.0F;
+    
+    // TODO: add files for model?
+
     // Start is called before the first frame update
     void Start()
     {
-        // rigidbody = GetComponent<Rigidbody>();
-        // use AddForce on the rigid body 
     }
 
     // Update is called once per frame
@@ -24,32 +29,32 @@ public class Player : MonoBehaviour
             // forward and backward movement
             if (Input.GetKey(KeyCode.W))
             {
-                rigidBody.AddForce(Vector3.forward * movementSpeed);
+                rigidBody.AddForce(Vector3.forward * movementSpeed * speedMult);
                 
             }
             if (Input.GetKey(KeyCode.S))
             {
-                rigidBody.AddForce(Vector3.back * movementSpeed);
+                rigidBody.AddForce(Vector3.back * movementSpeed * speedMult);
             }
 
             // leftward and rightward movement
             if (Input.GetKey(KeyCode.A))
             {
-                rigidBody.AddForce(Vector3.left * movementSpeed);
+                rigidBody.AddForce(Vector3.left * movementSpeed * speedMult);
             }
             if (Input.GetKey(KeyCode.D))
             {
-                rigidBody.AddForce(Vector3.right * movementSpeed);
+                rigidBody.AddForce(Vector3.right * movementSpeed * speedMult);
             }
 
             // upward and downward movement
             if (Input.GetKey(KeyCode.Q))
             {
-                rigidBody.AddForce(Vector3.up * movementSpeed);
+                rigidBody.AddForce(Vector3.up * movementSpeed * speedMult);
             }
             if (Input.GetKey(KeyCode.E))
             {
-                rigidBody.AddForce(Vector3.down * movementSpeed);
+                rigidBody.AddForce(Vector3.down * movementSpeed * speedMult);
             }
         }
         else
@@ -57,31 +62,31 @@ public class Player : MonoBehaviour
             // forward and backward movement
             if (Input.GetKey(KeyCode.W))
             {
-                transform.Translate(new Vector3(0, 0, movementSpeed * Time.deltaTime));
+                transform.Translate(new Vector3(0, 0, movementSpeed * speedMult * Time.deltaTime));
             }
             else if (Input.GetKey(KeyCode.S))
             {
-                transform.Translate(new Vector3(0, 0, -movementSpeed * Time.deltaTime));
+                transform.Translate(new Vector3(0, 0, -movementSpeed * speedMult * Time.deltaTime));
             }
 
             // leftward and rightward movement
             if (Input.GetKey(KeyCode.A))
             {
-                transform.Translate(new Vector3(-movementSpeed * Time.deltaTime, 0, 0));
+                transform.Translate(new Vector3(-movementSpeed * speedMult * Time.deltaTime, 0, 0));
             }
             else if (Input.GetKey(KeyCode.D))
             {
-                transform.Translate(new Vector3(movementSpeed * Time.deltaTime, 0, 0));
+                transform.Translate(new Vector3(movementSpeed * speedMult * Time.deltaTime, 0, 0));
             }
 
             // upward and downward movement
             if (Input.GetKey(KeyCode.Q))
             {
-                transform.Translate(new Vector3(0, movementSpeed * Time.deltaTime, 0));
+                transform.Translate(new Vector3(0, movementSpeed * speedMult * Time.deltaTime, 0));
             }
             else if (Input.GetKey(KeyCode.E))
             {
-                transform.Translate(new Vector3(0, -movementSpeed * Time.deltaTime, 0));
+                transform.Translate(new Vector3(0, -movementSpeed * speedMult * Time.deltaTime, 0));
             }
         }
     }
