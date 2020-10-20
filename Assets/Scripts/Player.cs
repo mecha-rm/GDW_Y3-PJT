dc9fb55f689e7b87930aad1474031aaf69e8c282
Roderick Montague <roderick.montague243@gmail.com>
2020-10-19 21:36:20 -0400

    FollowerCamera
    A camera that follows an object has been added. Having it properly readjust when the player moves in a new direction hasn't been added yet.
    Also some basic UI stuff like time and score have been added, but there is no functionality yet.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // the model that represents the player
+    public GameObject model;
+
+    // the player number
+    public int playerNumber = 1;
+
     public Rigidbody rigidBody;
     public float movementSpeed = 10.0F;
     public bool momentumMovement = false;
 
     // camera controls
     public Camera camera; // the player's camera
-    public Vector3 camDefaultDist = new Vector3(0, 3, 10); // camera distance from player
-    public Vector3 camDefaultRot = new Vector3(0, 0, 0); // the camera's default orientation
-     
+    private Vector3 direcVec; // the vector direction
+    private Vector3 lastPos; // the player's previous position
+
     // // saves the rotation of the camera
     // private Vector3 camRot = new Vector3(0.0F, 0.0F, 0.0F);
     // private Vector3 rotSpeed = new Vector3(100.0F, 100.0F, 0.0F);
@@ -24,7 +30,7 @@ public class Player : MonoBehaviour
     protected float knockbackMult = 1.0F;
     protected float jumpMult = 1.0F;
     protected float defenseMult = 1.0F;
-    
+
     // TODO: add files for model?
 
     // Start is called before the first frame update
@@ -37,74 +43,118 @@ public class Player : MonoBehaviour
     {
         if (momentumMovement)
         {
+            // TODO: factor in deltaTime
             // forward and backward movement
             if (Input.GetKey(KeyCode.W))
             {
-                rigidBody.AddForce(Vector3.forward * movementSpeed * speedMult);
-                
+                Vector3 force = Vector3.forward * movementSpeed * speedMult;
+                rigidBody.AddForce(force);
+                direcVec += force;
+
             }
             if (Input.GetKey(KeyCode.S))
             {
-                rigidBody.AddForce(Vector3.back * movementSpeed * speedMult);
+                Vector3 force = Vector3.back * movementSpeed * speedMult;
+                rigidBody.AddForce(force);
+                direcVec += force;
             }
 
             // leftward and rightward movement
             if (Input.GetKey(KeyCode.A))
             {
-                rigidBody.AddForce(Vector3.left * movementSpeed * speedMult);
+                Vector3 force = Vector3.left * movementSpeed * speedMult;
+                rigidBody.AddForce(force);
+                direcVec += force;
             }
             if (Input.GetKey(KeyCode.D))
             {
-                rigidBody.AddForce(Vector3.right * movementSpeed * speedMult);
+                Vector3 force = Vector3.right * movementSpeed * speedMult;
+                rigidBody.AddForce(force);
+                direcVec += force;
             }
 
             // upward and downward movement
             if (Input.GetKey(KeyCode.Q))
             {
-                rigidBody.AddForce(Vector3.up * movementSpeed * speedMult);
+                Vector3 force = Vector3.up * movementSpeed * speedMult;
+                rigidBody.AddForce(force);
+                direcVec += force;
             }
             if (Input.GetKey(KeyCode.E))
             {
-                rigidBody.AddForce(Vector3.down * movementSpeed * speedMult);
+                Vector3 force = Vector3.down * movementSpeed * speedMult;
+                rigidBody.AddForce(force);
+                direcVec += force;
             }
+
+            // normalize the direction vector.
+            direcVec.Normalize();
         }
         else
         {
             // forward and backward movement
             if (Input.GetKey(KeyCode.W))
             {
-                transform.Translate(new Vector3(0, 0, movementSpeed * speedMult * Time.deltaTime));
+                Vector3 shift = new Vector3(0, 0, movementSpeed * speedMult * Time.deltaTime);
+                transform.Translate(shift);
+                direcVec += shift;
             }
             else if (Input.GetKey(KeyCode.S))
             {
-                transform.Translate(new Vector3(0, 0, -movementSpeed * speedMult * Time.deltaTime));
+                Vector3 shift = new Vector3(0, 0, -movementSpeed * speedMult * Time.deltaTime);
+                transform.Translate(shift);
+                direcVec += shift;
             }
 
             // leftward and rightward movement
             if (Input.GetKey(KeyCode.A))
             {
-                transform.Translate(new Vector3(-movementSpeed * speedMult * Time.deltaTime, 0, 0));
+                Vector3 shift = new Vector3(-movementSpeed * speedMult * Time.deltaTime, 0, 0);
+                transform.Translate(shift);
+                direcVec += shift;
             }
             else if (Input.GetKey(KeyCode.D))
             {
-                transform.Translate(new Vector3(movementSpeed * speedMult * Time.deltaTime, 0, 0));
+                Vector3 shift = new Vector3(movementSpeed * speedMult * Time.deltaTime, 0, 0);
+                transform.Translate(shift);
+                direcVec += shift;
             }
 
             // upward and downward movement
             if (Input.GetKey(KeyCode.Q))
             {
-                transform.Translate(new Vector3(0, movementSpeed * speedMult * Time.deltaTime, 0));
+                Vector3 shift = new Vector3(0, movementSpeed * speedMult * Time.deltaTime, 0);
+                transform.Translate(shift);
+                direcVec += shift;
             }
             else if (Input.GetKey(KeyCode.E))
             {
-                transform.Translate(new Vector3(0, -movementSpeed * speedMult * Time.deltaTime, 0));
+                Vector3 shift = new Vector3(0, -movementSpeed * speedMult * Time.deltaTime, 0);
+                transform.Translate(shift);
+                direcVec += shift;
             }
         }
 
-
-        // if()
+        // gets the change in position - player has moved.
+        // if(transform.position != lastPos)
         // {
+        //     // we only care about the x and z axis position
+        //     Vector2 v0 = new Vector3(transform.position.x, transform.position.z);
+        //     Vector2 v1 = new Vector3(lastPos.x, lastPos.z);
+        //     Vector2 v2 = v1 - v0;
+        //     
+        //     
+        //     float angle = Vector3.Angle(v0, v1); // angle between the player's old and new position
         // 
+        //     // gets follower camera component.
+        //     FollowerCamera fc = camera.GetComponent<FollowerCamera>();
+        //     if(fc != null)
+        //     {
+        //         fc.SetRotation(0.0F, -angle, 0.0F); // changes camera rotation
+        //     }
         // }
+
+        // saves the player's current position
+        lastPos = transform.position;
     }
 }
