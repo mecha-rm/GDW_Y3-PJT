302f5fa6db23c74bb7295f22693c48920284f338
Roderick Montague <roderick.montague243@gmail.com>
2020-11-06 22:50:02 -0500

    Improved Movement, and Scenes Added
    Start and end scenes have been added, but they just have buttons that lead back to gameplay. Movement has been changed so that W moves you forward, A/D rotate the player, S moves them backward/slows them down, and the down arrow quickly turns the player around (which should use lerp some time down the road). Also added a jump with up arrow/space bar, but it's a bit finicky. I plan on improving the user interface, adding the run animation, and adding the main gameplay loop.

--- Assets/Scripts/Managers/GameplayManager.cs
+++ Assets/Scripts/Managers/GameplayManager.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class GameplayManager : MonoBehaviour
 {
+    public float score = 100.0F;
+
+    // the four players
+    PlayerObject p1 = null;
+    PlayerObject p2 = null;
+    PlayerObject p3 = null;
+    PlayerObject p4 = null;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/PlayerObject.cs
+++ Assets/Scripts/PlayerObject.cs
@@ -1,6 +1,7 @@
 ﻿// class for the player
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 
 public class PlayerObject : MonoBehaviour
@@ -13,7 +14,9 @@ public class PlayerObject : MonoBehaviour
 
     // the rigidbody for the player.
     private Rigidbody rigidBody; // maybe make this private since the start() function gets it?
-    public float movementSpeed = 10.0F;
+    public float movementSpeed = 12.0F;
+    public float jumpForce = 20000.0F;
+    public float backupFactor = 0.5F;
     public bool momentumMovement = false;
 
     // camera controls
@@ -32,8 +35,9 @@ public class PlayerObject : MonoBehaviour
 
     // // saves the rotation of the camera
     // private Vector3 camRot = new Vector3(0.0F, 0.0F, 0.0F);
-    // private Vector3 rotSpeed = new Vector3(100.0F, 100.0F, 0.0F);
-    // private Vector2 xRotLimit = new Vector2(-50.0F, 50.0F);
+    private Vector3 rotSpeed = new Vector3(150.0F, 150.0F, 150.0F);
+    private Vector2 xRotLimit = new Vector2(-50.0F, 50.0F);
+    private float maxVelocity = 50.0F;
 
     // animal characteristics
     // TODO: set these to protected for production build
@@ -151,75 +155,208 @@ public class PlayerObject : MonoBehaviour
     {
         if (momentumMovement)
         {
-            // TODO: factor in deltaTime
-            // forward and backward movement
-            if (Input.GetKey(KeyCode.W))
+            // TODO: factor in deltaTime for movement
+            // TODO: lerp camera for rotation
+            
+            // Movement
             {
-                Vector3 force = Vector3.forward * movementSpeed * speedMult;
-                // Vector3 force = transform.forward * movementSpeed * speedMult;
-                rigidBody.AddForce(force);
-                direcVec += force;
-                
+                // forward and backward movement
+                if (Input.GetKey(KeyCode.W))
+                {
+                    // Vector3 force = Vector3.forward * movementSpeed * speedMult;
+                    // // Vector3 force = transform.forward * movementSpeed * speedMult;
+                    // rigidBody.AddForce(force);
+                    // direcVec += force;
+                    Vector3 force = transform.forward * movementSpeed * speedMult;
+                    rigidBody.AddForce(force);
+                    direcVec += force;
 
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                // TODO: maybe instead of moving down you just rotate the player.
-                Vector3 force = Vector3.back * movementSpeed * speedMult;
-                // Vector3 force = -transform.forward * movementSpeed * speedMult;
-                rigidBody.AddForce(force);
-                direcVec += force;
+                }
+                else if (Input.GetKey(KeyCode.S))
+                {
+                    // TODO: maybe instead of moving down you just rotate the player.
+                    // Vector3 force = Vector3.back * movementSpeed * speedMult;
+                    Vector3 force = -transform.forward * movementSpeed * backupFactor * speedMult;
+
+
+                    rigidBody.AddForce(force);
+
+                    direcVec += force;
+
+                    // TODO: rotate if not facing backward.
+
+                }
+
+                // leftward and rightward movement
+                if (Input.GetKey(KeyCode.A))
+                {
+                    // Vector3 force = Vector3.left * movementSpeed * speedMult;
+                    // Vector3 force = -transform.right * movementSpeed * speedMult;
+                    // rigidBody.AddForce(force);
+                    // direcVec += force;
+
+
+                    // if there is no velocity, set the player's rotation to -90 degrees.
+                    if(Input.GetKey(KeyCode.W)) // if the player is going foward
+                    {
+                        Vector3 force = -transform.right * movementSpeed * speedMult;
+                        rigidBody.AddForce(force);
+                        direcVec += force;
+
+                        transform.Rotate(Vector3.up, -rotSpeed.y * Time.deltaTime);
+                    }
+                    else // the player is not going forward so, rotate instead.
+                    {
+                        transform.Rotate(Vector3.up, -rotSpeed.y * Time.deltaTime);
+                    }
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    // Vector3 force = Vector3.right * movementSpeed * speedMult;
+                    // Vector3 force = transform.right * movementSpeed * speedMult;
+                    // rigidBody.AddForce(force);
+                    // direcVec += force;
+
+                    // if there is no velocity, set the player's rotation to -90 degrees.
+                    if (Input.GetKey(KeyCode.W)) // if the player is going foward
+                    {
+                        Vector3 force = transform.right * movementSpeed * speedMult;
+                        rigidBody.AddForce(force);
+                        direcVec += force;
+                        // transform.Rotate(Vector3.up, rotSpeed.y * Time.deltaTime);
+                    }
+                    else // the player is not going forward so, rotate instead.
+                    {
+                        transform.Rotate(Vector3.up, rotSpeed.y * Time.deltaTime);
+                    }
+                }
+
+                // unused
+                // upward and downward movement
+                // if (Input.GetKey(KeyCode.Q))
+                // {
+                //     Vector3 force = Vector3.up * movementSpeed * speedMult;
+                //     // Vector3 force = transform.up * movementSpeed * speedMult;
+                //     rigidBody.AddForce(force);
+                //     direcVec += force;
+                // }
+                // if (Input.GetKey(KeyCode.E))
+                // {
+                //     Vector3 force = Vector3.down * movementSpeed * speedMult;
+                //     // Vector3 force = -transform.up * movementSpeed * speedMult;
+                //     rigidBody.AddForce(force);
+                //     direcVec += force;
+                // }
             }
 
-            // leftward and rightward movement
-            if (Input.GetKey(KeyCode.A))
+            // Hard Rotation (Snap)
             {
-                Vector3 force = Vector3.left * movementSpeed * speedMult;
-                // Vector3 force = -transform.right * movementSpeed * speedMult;
-                rigidBody.AddForce(force);
-                direcVec += force;
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                Vector3 force = Vector3.right * movementSpeed * speedMult;
-                // Vector3 force = transform.right * movementSpeed * speedMult;
-                rigidBody.AddForce(force);
-                direcVec += force;
+                // rotate to the left (slow)
+                if(Input.GetKey(KeyCode.LeftArrow))
+                {
+                    transform.Rotate(Vector3.up, -rotSpeed.y * Time.deltaTime);
+                }
+                // rotate to the right
+                else if(Input.GetKey(KeyCode.RightArrow))
+                {
+                    transform.Rotate(Vector3.up, +rotSpeed.y * Time.deltaTime);
+                }
+
+
+                // hard rotation
+                // if (Input.GetKeyDown(KeyCode.LeftArrow))
+                // {
+                //     transform.Rotate(Vector3.up, -90.0F);
+                // }
+                // else if(Input.GetKeyDown(KeyCode.RightArrow))
+                // {
+                //     transform.Rotate(Vector3.up, 90.0F);
+                // }
+
+                // hard rotation
+                // judge player forward distance
+                // if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+                // {
+                //     // hard rotation 90 degrees to the left.
+                //     if (Input.GetKeyDown(KeyCode.LeftArrow))
+                //         transform.Rotate(Vector3.up, -90.0F);
+                // 
+                //     // hard rotation 90 degrees to the right.
+                //     if (Input.GetKeyDown(KeyCode.RightArrow))
+                //         transform.Rotate(Vector3.up, 90.0F);
+                // 
+                //     // gets the distance between the four axes to see which one the player is closest too.
+                //     
+                // 
+                //     float distF = Vector3.Distance(transform.forward, Vector3.forward);
+                //     float distB = Vector3.Distance(transform.forward, Vector3.back);
+                //     float distL = Vector3.Distance(transform.forward, Vector3.left);
+                //     float distR = Vector3.Distance(transform.forward, Vector3.right);
+                // 
+                //     // gets the shortest distance
+                //     float shortDist = Mathf.Min(distF, distB, distL, distR);
+                // 
+                //     
+                // 
+                //     // checks to see what distance the player is closest to.
+                //     if (shortDist == distF)
+                //         transform.eulerAngles = new Vector3(0.0F, 0.0F, 0.0F);
+                //     else if (shortDist == distB)
+                //         transform.eulerAngles = new Vector3(0.0F, 90.0F, 0.0F);
+                //     else if (shortDist == distL)
+                //         transform.eulerAngles = new Vector3(0.0F, 180.0F, 0.0F);
+                //     else if (shortDist == distR)
+                //         transform.eulerAngles = new Vector3(0.0F, 270.0F, 0.0F);
+                // 
+                // 
+                // }
+
+                // turn backwards
+                if(Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    transform.Rotate(Vector3.up, 180.0F);
+                }
             }
 
-            // upward and downward movement
-            if (Input.GetKey(KeyCode.Q))
+            // jump
             {
-                Vector3 force = Vector3.up * movementSpeed * speedMult;
-                // Vector3 force = transform.up * movementSpeed * speedMult;
-                rigidBody.AddForce(force);
-                direcVec += force;
-            }
-            if (Input.GetKey(KeyCode.E))
-            {
-                Vector3 force = Vector3.down * movementSpeed * speedMult;
-                // Vector3 force = -transform.up * movementSpeed * speedMult;
-                rigidBody.AddForce(force);
-                direcVec += force;
+                // TODO: check to see if this effects slopes.
+                if((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)) && rigidBody.velocity.y == 0.0F)
+                {
+                    // transform.position = new Vector3(transform.position.x, transform.position.y + 0.1F, transform.position.z);
+                    // rigidBody.AddExplosionForce(jumpForce * 1000.0f, transform.position, 1.0F);
+                    rigidBody.AddForce(Vector3.up * jumpForce * Time.deltaTime);
+                    // rigidBody.AddForce(Vector3.up * jumpForce);
+
+                }
             }
 
             // gets the angle
-            {
-                Vector3 currVelo = rigidBody.velocity;
-                if(currVelo != new Vector3())
-                {
-                    transform.LookAt(transform.position + currVelo);
-                    transform.rotation = new Quaternion(0.0f, transform.rotation.y, 0.0F, 1.0F);
-
-                    // camera.rotation = transform.rotation.eulerAngles;
-                    // camera.rotation.x = 0.0F;
-                    // camera.rotation.y *= -1;
-                    // camera.rotation.z = 0.0F;
-                }
-                
-            }
+            // {
+            //     Vector3 currVelo = rigidBody.velocity;
+            //     if(currVelo != new Vector3())
+            //     {
+            //         transform.LookAt(transform.position + currVelo);
+            //         transform.rotation = new Quaternion(0.0f, transform.rotation.y, 0.0F, 1.0F);
+            // 
+            //         // camera.rotation = transform.rotation.eulerAngles;
+            //         // camera.rotation.x = 0.0F;
+            //         // camera.rotation.y *= -1;
+            //         // camera.rotation.z = 0.0F;
+            //     }
+            //     
+            // }
 
             // direcVec.Normalize();
+
+            // caps velocity
+            if (rigidBody.velocity.magnitude > maxVelocity)
+            {
+                rigidBody.velocity = rigidBody.velocity.normalized * maxVelocity;
+
+                // float revForce = rigidBody.velocity.magnitude - maxVelocity;
+                // rigidBody.AddForce(rigidBody.velocity.normalized * -1 * revForce);
+            }
            
         }
         else
