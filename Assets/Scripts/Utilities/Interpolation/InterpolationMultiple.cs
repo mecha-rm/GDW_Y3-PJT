30b5cd6a88233691819240a6571f451ba3c9878a
Roderick Montague <roderick.montague243@gmail.com>
2020-12-28 23:44:41 -0500

    Starting Interpolation Classes
    I started some interpolation classes  that will be implemented later. I'm probably going to pivot to something else, but I wanted to get the base setup now.

---
+++ Assets/Scripts/Utilities/Interpolation/InterpolationMultiple.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO: implement interpolation for multiple objects along the same path.
+public class InterpolationMultiple : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
