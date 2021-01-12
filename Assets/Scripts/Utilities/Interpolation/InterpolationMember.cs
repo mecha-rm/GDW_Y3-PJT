afc40683050233e41845b2e6b42868f9f4655273
Roderick Montague <roderick.montague243@gmail.com>
2021-01-12 18:41:15 -0500

    Added Interpolation
    Interpolation scripts have been added. I haven't tested it yet, but it's there. I was thinking the bats could use it, but I've yet to try it.

---
+++ Assets/Scripts/Utilities/Interpolation/InterpolationMember.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// member of interpolation list.
+public class InterpolationMember : MonoBehaviour
+{
+    // starting position
+    public Vector3 startPos;
+
+    // index of interpolation list
+    public int index;
+
+    // t value
+    public float t;
+
+    // if the member should be paused.
+    public bool paused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Utilities/Interpolation/InterpolatonSingle.cs
+++ Assets/Scripts/Utilities/Interpolation/InterpolatonSingle.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 // interpolation (single)
 public class InterpolatonSingle : Interpolation // MonoBehaviour
 {
-    // TODO: add interpolation options.
+    // interpolation type
+    // NOTE: if 'Interpolate()' is overridden, this variable is disregarded.
+    public interType mode;
 
     // time
     public float t = 0;
@@ -17,40 +19,77 @@ public class InterpolatonSingle : Interpolation // MonoBehaviour
     public bool addStartPos;
 
     // current position
-    private Vector3 currPos;
+    private Vector3 startPos;
 
     // index of the travel points list.
     public int index;
 
+    // if 'true', the interpolation loops.
+    public bool loop = true;
+
+    // if 'true', the interpolation process is paused.
+    public bool paused = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        currPos = transform.position;
+        startPos = transform.position;
 
         // adds the starting position
         if (addStartPos)
             travelPoints.Add(transform.position);
     }
 
+    // function for interpolation
+    // override this function to change the interpolation type
+    public virtual void Interpolate(Vector3 v1, Vector3 v2, float t)
+    {
+        transform.position = InterpolateByType(mode, v1, v2, t);
+        // transform.position = Vector3.Lerp(v1, v2, t);
+    }
+
+    // checks to see if the path is paused.
+    public bool IsPaused()
+    {
+        return paused;
+    }
+
+    // sets whether the process should be paused or not.
+    public void SetPause(bool pause)
+    {
+        paused = pause;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime;
-        t = Mathf.Clamp(t, 0.0F, 1.0F);
-
-        // clamps the index value
-        index = Mathf.Clamp(index, 0, travelPoints.Count);
-
-        transform.position = Vector3.Lerp(currPos, travelPoints[index], t);
-
-        // end of line reached
-        if (t >= 1.0F)
-            index++;
-
-        // list has been gone through, so go back to the beginning.
-        if(index >= travelPoints.Count)
+        if (!paused)
         {
-            index = 0;
+            t += Time.deltaTime;
+            t = Mathf.Clamp(t, 0.0F, 1.0F);
+
+            // clamps the index value
+            index = Mathf.Clamp(index, 0, travelPoints.Count);
+
+            // transform.position = Vector3.Lerp(currPos, travelPoints[index], t);
+            Interpolate(startPos, travelPoints[index], t);
+
+            // end of segment reached
+            if (t >= 1.0F)
+            {
+                index++;
+                t = 0.0F;
+            }
+
+            // list has been gone through, so go back to the beginning.
+            if (index >= travelPoints.Count)
+            {
+                index = 0;
+
+                // if the path shouldn't loop, it pauses the interpolation after it's down.
+                if (!loop)
+                    paused = true;
+            }
         }
     }
 }
