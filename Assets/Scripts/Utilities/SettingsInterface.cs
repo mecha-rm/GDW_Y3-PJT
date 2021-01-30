70914d308c19938a26478337b0a9537ed5075615
Roderick Montague <roderick.montague243@gmail.com>
2021-01-29 19:04:43 -0500

    Fixed Menu + Added Settings Script (not implemented yet)
    The menu has been fixed so that the buttons go to the right submenus. I also prepared a setting script, which isn't implemented yet, but is there for later.

---
+++ Assets/Scripts/Utilities/SettingsInterface.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// script for settings
+// interfaces with the SettingsSingleton for the menu.
+public class SettingsInterface : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // gets the volume
+    public float GetMasterVolume()
+    {
+        return SettingsSingleton.GetInstance().GetMasterVolume();
+    }
+
+    // sets the volume
+    public void SetMasterVolume(float vol)
+    {
+        SettingsSingleton.GetInstance().SetMasterVolume(vol);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/Utilities/SettingsSingleton.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// setting singleton for mass game options
+public class SettingsSingleton
+{
+    // instance of singleton
+    private static SettingsSingleton instance = null;
+
+    // volume control
+    // master volume
+    private float masterVolume = 1.0F;
+
+    // constructor
+    private SettingsSingleton()
+    {
+        Start();
+    }
+
+    // gets the instance
+    public static SettingsSingleton GetInstance()
+    {
+        // no instance generated
+        if (instance == null) 
+        {
+            // generates instance
+            instance = new SettingsSingleton(); 
+        }
+
+        return instance;
+    }
+
+    // start function
+    void Start()
+    {
+    }
+
+    // gets the volume
+    public float GetMasterVolume()
+    {
+        return masterVolume;
+    }
+
+    // sets the volume
+    public void SetMasterVolume(float vol)
+    {
+        masterVolume = Mathf.Clamp(vol, 0.0F, 1.0F);
+        AudioListener.volume = masterVolume;
+    }
+}
