f8f300c13bdcb34f1d37bd5eb7e135030d85dccd
Roderick Montague <roderick.montague243@gmail.com>
2021-02-09 20:18:21 -0500

    Worked on Arrow Movement (Still WIP)
    The arrow movement for tutorial 1 works now, but it does not jump to the next step like it should. This should be fixed for later or just disabled.

--- Assets/Scripts/Tutorial/ArrowMovement.cs
+++ Assets/Scripts/Tutorial/ArrowMovement.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// movement for moving an arrow.
 public class ArrowMovement : MonoBehaviour
 {
+    // should be attached to the moving object.
     // transform
-    public Transform movingArrow;   
+    public Transform movingArrow;
+
+    // minimum and maximum Y for movement.
+    float minY = 5.0F;
+    float maxY = 15.0F;
 
     // speed
-    public float speed = 5.0f;
+    public float speed = 10.0f;
 
-    // end point
-    public Vector3 endPoint;
+    // start and end point
+    // public Vector3 startPoint;
+    // public Vector3 endPoint;
+
+    // time value
+    // private float t;
+    // 
+    // // direction
+    // private bool forward = true;
 
     // Start is called before the first frame update
     void Start()
@@ -24,15 +37,49 @@ public class ArrowMovement : MonoBehaviour
     void Update()
     {
         //Moves arrow up and down using LERP
-        if(movingArrow.position.y > 15.0f)
+        if(movingArrow.position.y > maxY) // 15.0F
         {
-            speed = 10.0f;
+            // speed = 10.0f;
+            speed *= -1;
         }
-        if(movingArrow.position.y < 5.0f)
+        if(movingArrow.position.y < minY) // 5.0F
         {
-            speed = -10.0f;
+            // speed = -10.0f;
+            speed *= -1;
         }
 
+        // the rotation must be cleared in order for the arrow to move in the right direction.
+        // so the rotation gets removed for the translation, then gets added back in.
+        Quaternion rot = movingArrow.rotation;
+        movingArrow.rotation = new Quaternion(0, 0, 0, 1);
         movingArrow.Translate(0, speed * Time.deltaTime, 0);
+        movingArrow.rotation = rot;
+
+      
+
+        // ver. 2
+        // Vector3 direcNorm = (endPoint - transform.position).normalized;
+        // transform.Translate(direcNorm * speed * Time.deltaTime);
+
+        // ver. 3
+        // new position
+        // Vector3 newPos;
+        // 
+        // // delta time variable
+        // t += Time.deltaTime * speed;
+        // t = Mathf.Clamp(t, 0, 1);
+        // 
+        // // determines what direction should be gone in.
+        // if (forward)
+        //     newPos = Vector3.Lerp(startPoint, endPoint, t);
+        // else
+        //     newPos = Vector3.Lerp(endPoint, startPoint, t);
+        // 
+        // // reverse direction
+        // if(t >= 1.0F)
+        // {
+        //     t = 0.0F;
+        //     forward = !forward;
+        // }
     }
 }
