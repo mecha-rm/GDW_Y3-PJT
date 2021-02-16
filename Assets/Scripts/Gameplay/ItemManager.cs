08c7010f0de1063e2a838269d682f6b6fc387494
Roderick Montague <roderick.montague243@gmail.com>
2021-02-16 14:34:35 -0500

    GameBuilder Somewhat Works - Working on Item Implementation
    The GameBuilder somewhat works. Some of the default objects in the scene seem to interfere with it (e.g. there are now two GameplayManagers when the game loads), but it mostly works now. I'm going to work on making item drops for the game now.

---
+++ Assets/Scripts/Gameplay/ItemManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    // list of items in the pool
+    List<GameplayItem> itemPool;
+
+    // the item prefab
+    GameObject ItemPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
