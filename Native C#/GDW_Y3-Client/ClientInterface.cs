236d5e753bf36e38f0068a9bcb2a7d7308a50223
Roderick Montague <roderick.montague243@gmail.com>
2021-03-09 23:50:11 -0500

    Working on Two-Way Communication Between Server and Client
    The server and client can communicate, but two way communication is needed.

---
+++ Native C#/GDW_Y3-Client/ClientInterface.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkLibrary
+{
+    // client interface
+    class ClientInterface
+    {
+        // gets the server
+        private static GDW_Y3_Client.UdpClient client = new GDW_Y3_Client.UdpClient();
+
+        // gets the buffer size.
+        public static int GetBufferSize()
+        {
+            return client.GetBufferSize();
+        }
+
+        // sets the buffer size.
+        public static void SetBufferSize(int size)
+        {
+            client.SetBufferSize(size);
+        }
+
+        // gets the buffer data
+        public static byte[] GetBufferData()
+        {
+            return client.GetBufferData();
+        }
+
+        // gets the ip address as a string
+        public static String GetIPAddress()
+        {
+            return client.GetIPAddress();
+        }
+
+        // getter for the send timeout
+        public static int GetSendTimeout()
+        {
+            return client.GetSendTimeout();
+        }
+
+        // setter for send tiemout
+        public static void SetSendTimeout(int newSt)
+        {
+            client.SetSendTimeout(newSt);
+        }
+
+        // gets the receiver timeout.
+        public static int GetReceiverTimeout()
+        {
+            return client.GetReceiverTimeout();
+        }
+
+        // sets the receiver timeout.
+        public static void SetReceiverTimeout(int newRt)
+        {
+            client.SetReceiverTimeout(newRt);
+        }
+
+        // checks to see if the server is running
+        public static bool IsRunning()
+        {
+            return client.IsRunning();
+        }
+
+        // checks to see if the server is connected.
+        public static bool IsConnected()
+        {
+            return client.IsConnected();
+        }
+
+        // run the server project.
+        public static void RunClient()
+        {
+            client.RunClient();
+        }
+
+        // updates server
+        public static void Update()
+        {
+            client.Update();
+        }
+
+        // shuts down the server.
+        public static void ShutdownClient()
+        {
+            client.ShutdownClient();
+        }
+    }
+}
