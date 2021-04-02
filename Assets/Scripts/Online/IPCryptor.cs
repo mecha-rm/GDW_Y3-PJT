1c615470d90b37229e0339833d71e729efe84ede
Roderick Montague <roderick.montague243@gmail.com>
2021-04-02 02:22:50 -0400

    Working on Online Play
    Working on multiplayer screen.

---
+++ Assets/Scripts/Online/IPCryptor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+
+// the ip encryptor and decryptor
+public class IPCryptor : MonoBehaviour
+{
+    // gets the system IP address.
+    public static string GetSystemIPAddress()
+    {
+        return GetSystemIPAddress(1);
+    }
+
+    // gets the system IP address.
+    // provides 'address list index'
+    public static string GetSystemIPAddress(int alIndex)
+    {
+        IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress ip = host.AddressList[alIndex]; // get IP address from list ([1])
+        return ip.ToString();
+    }
+
+    // generates a system ip address.
+    public static IPAddress GenerateSystemIPAddress()
+    {
+        IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress ip = host.AddressList[1]; // get IP address from list
+        return ip;
+    }
+
+    // encrypts the ip address to create a room code.
+    public static string EncryptIP(string ipD)
+    {
+        string ipE = ipD;
+
+        // TODO: encrypt ip address
+
+        return ipE;
+    }
+
+    // decrypts a room to create a room code.
+    public static string DecryptIP(string ipE)
+    {
+        string ipD = ipE;
+
+        // TODO: decrypt ip address
+
+        return ipD;
+    }
+}
